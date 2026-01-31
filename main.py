f49d41c4858ce558ecc1d45c09ea93aad8aa32ff
krityam <krityamsingh100@gmail.com>
2026-01-31 11:55:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ CHANNEL_USERNAMES = ["Capture_Talks", "BLACKCLV"]
 CHANNEL_LINKS = ["https://t.me/Capture_Talks", "https://t.me/BLACKCLV"]
 
 # Group Configuration (MUST SET THIS - get from @RawDataBot)
-GROUP_ID = None  # ⚠️ SET THIS: Example -1001234567890
+GROUP_ID = -1002313549356  # ⚠️ SET THIS: Example -1001234567890
 
 # Bot settings
 INSIDE_ADS_BOT = "InsideAds_bot"
@@ -828,3 +828,4 @@ if __name__ == "__main__":
         import time
         time.sleep(10)
         loop.run_until_complete(main())
+
