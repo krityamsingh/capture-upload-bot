b45d250114848ef91bdfb8293f1729e51a9f8c8c
SDWZORO <fosownerzoro@gmail.com>
2026-01-31 17:55:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,22 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot Configuration
-API_ID = int(os.getenv("API_ID", 26676741))
-API_HASH = os.getenv("API_HASH", "6fbc29f23c15bdb0c7fbbefe65c9193a")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8260866955:AAFyZ2LyG7tmHkOzDBXoZkx8sgcx1U3d7dE")
+API_ID = 26676741
+API_HASH = "6fbc29f23c15bdb0c7fbbefe65c9193a"
+BOT_TOKEN = "8400868432:AAELK0oQXqxXlZJbusLn2QsgIwYkG6-cqss"
 TOKEN = BOT_TOKEN
 
 # Database Configuration
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://krityamwixs:krityamwixs@cluster0.oqvxe2t.mongodb.net/?appName=Cluster0")
+MONGO_URL = "mongodb+srv://krityamwixs:krityamwixs@cluster0.oqvxe2t.mongodb.net/?appName=Cluster0"
 
 # Catbox.moe Configuration
-CATBOX_API_KEY = os.getenv("CATBOX_API_KEY", "")
+CATBOX_API_KEY = ""
 
-# Owner Configuration
-OWNER_ID = int(os.getenv("OWNER_ID", 8301883098))
-
-# Permanent Broadcast Channels (These will always be used)
-PERMANENT_BROADCAST_CHANNELS = [-1003663151888, -1003364380308]
+# Owner Configuration - HARDCODED
+OWNER_ID = 8301883098
 
 # Upload Settings
 MAX_FILE_SIZE = 50 * 1024 * 1024  # 50MB
@@ -31,8 +28,4 @@ CATBOX_TIMEOUT = 30  # seconds
 CATBOX_MAX_RETRIES = 3
 
 # Logging Configuration
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
-
-
-
-
+LOG_LEVEL = "INFO"
